39c77b39c2fcc6623fa93ec2be77893c9dc5280a
Blackoutburst <blackoutburst@hotmail.com>
2024-09-29 01:14:39 +0200

    fixed code

--- js/shaders/tracer.js
+++ js/shaders/tracer.js
@@ -18,8 +18,7 @@ precision highp float;
 #define S32_MAX 2147483647.0
 #define RENDER_PASS 200
 #define BOUNCE_COUNT 20
-#define FRAMES 2
-#define DIVERGENCE 10.0
+#define DIVERGENCE 5.0
 
 uniform vec2 resolution;
 uniform float time;
@@ -57,14 +56,15 @@ struct Sphere {
     RayTracingMaterial material;
 };
 
-int NumSpheres = 6;
-const Sphere Spheres[6] = Sphere[6](
-    Sphere(vec3(-2.0, 0.0, -4.0), 1.0, RayTracingMaterial(vec4(1.0, 0.5, 0.0, 1.0), vec4(0.0), 0.0, 1.0, 0.1, vec4(1.0))),
-    Sphere(vec3(2.0, 0.0, -4.0), 1.0, RayTracingMaterial(vec4(0.0, 0.5, 1.0, 1.0), vec4(0.0), 0.0, 1.0, 0.3, vec4(1.0))),
+int NumSpheres = 7;
+const Sphere Spheres[7] = Sphere[7](
+    Sphere(vec3(-2.0, 0.1, -4.0), 1.0, RayTracingMaterial(vec4(1.0, 0.5, 0.0, 1.0), vec4(0.0), 0.0, 1.0, 0.5, vec4(1.0))),
+    Sphere(vec3(2.0, 0.1, -4.0), 1.0, RayTracingMaterial(vec4(0.0, 0.5, 1.0, 1.0), vec4(0.0), 0.0, 1.0, 0.1, vec4(1.0))),
     Sphere(vec3(0.0, 1.0, -5.0), 1.0, RayTracingMaterial(vec4(1.0), vec4(0.0), 0.0, 0.5, 1.0, vec4(1.0))),
-    Sphere(vec3(0.0, -1.1, -3.0), 0.5, RayTracingMaterial(vec4(1.0), vec4(0.0, 1.0, 0.0, 1.0), 5.0, 0.0, 0.0, vec4(1.0))),
-    Sphere(vec3(0.0, -20.0, -20.0), 25.0, RayTracingMaterial(vec4(1.0, 0.4, 1.0, 1.0), vec4(0.0), 0.0, 1.0, 0.1, vec4(1.0))),
-    Sphere(vec3(0.0, 30.0, -30.0), 25.0, RayTracingMaterial(vec4(0.0), vec4(1.0), 5.0, 1.0, 0.0, vec4(1.0)))
+    Sphere(vec3(0.0, -1.1, -3.0), 0.5, RayTracingMaterial(vec4(1.0), vec4(0.0, 1.0, 0.0, 1.0), 0.0, 1.0, 1.0, vec4(1.0))),
+    Sphere(vec3(0.0, -20.0, -20.0), 25.0, RayTracingMaterial(vec4(1.0, 0.4, 1.0, 1.0), vec4(0.0), 0.0, 1.0, 0.8, vec4(1.0))),
+    Sphere(vec3(0.0, 80.0, -30.0), 25.0, RayTracingMaterial(vec4(0.0), vec4(1.0), 10.0, 1.0, 0.0, vec4(1.0))),
+    Sphere(vec3(0.0, 30.0, 30.0), 25.0, RayTracingMaterial(vec4(0.0), vec4(1.0), 1.0, 1.0, 0.0, vec4(1.0)))
 );
 
 HitInfo RaySphere(Ray ray, vec3 sphereCenter, float sphereRadius) {
@@ -108,39 +108,29 @@ HitInfo CalculateRayCollision(Ray ray) {
     return closestHit;
 }
 
-float RandomValue(inout float state) {
-   return fract(sin(state += 0.1+time)*43758.5453123);
+float RandomValue(inout uint state) {
+    state = state * 747796405u + 2891336453u;
+    uint word = ((state >> ((state >> 28u) + 4u)) ^ state) * 277803737u;
+    word = (word >> 22u) ^ word;
+    return float(word) / 4294967295.0;
 }
 
-float RandomValueNormalDistribution(inout float state) {
+float RandomValueNormalDistribution(inout uint state) {
     float theta = 2.0 * 3.1415926 * RandomValue(state);
     float rho = sqrt(-2.0 * log(RandomValue(state)));
     return rho * cos(theta);
 }
 
-vec3 RandomDirection(inout float state) {
-    for (int i = 0; i < RENDER_PASS; i++) {
-        float x = RandomValueNormalDistribution(state);
-        float y = RandomValueNormalDistribution(state);
-        float z = RandomValueNormalDistribution(state);
-        vec3 pointInCube = vec3(x, y, z);
-        float sqrDstFromCenter = dot(pointInCube, pointInCube);
+vec3 RandomDirection(inout uint state) {
+    float x = RandomValueNormalDistribution(state);
+    float y = RandomValueNormalDistribution(state);
+    float z = RandomValueNormalDistribution(state);
 
-        if (sqrDstFromCenter <= 1.0) {
-            return pointInCube / sqrt(sqrDstFromCenter);
-        }
-    }
-
-    return vec3(0.0);
+    return normalize(vec3(x, y, z));
 }
 
-vec3 RandomHemisphereDirection(vec3 normal, inout float state) {
-    vec3 dir = RandomDirection(state);
-    return dir * sign(dot(normal, dir));
-}
-
-vec3 Trace(Ray ray, inout float state) {
-    vec4 incomingLight = vec4(0.0);
+vec3 Trace(Ray ray, inout uint state) {
+    vec4 incomingLight = vec4(0.1);
     vec4 rayColor = vec4(1.0);
     for (int i = 0; i <= BOUNCE_COUNT; i++) {
         HitInfo hitInfo = CalculateRayCollision(ray);
@@ -164,7 +154,7 @@ vec3 Trace(Ray ray, inout float state) {
     return incomingLight.xyz;
 }
 
-vec2 RandomPointInCircle(inout float state) {
+vec2 RandomPointInCircle(inout uint state) {
     float angle = RandomValue(state) * 2.0 * 3.1415926;
     vec2 pointOnCircle = vec2(cos(angle), sin(angle));
     return pointOnCircle * sqrt(RandomValue(state));
@@ -175,10 +165,10 @@ void main() {
     ndc.x *= resolution.x / resolution.y;
 
     vec2 numPixels = resolution.xy;
-    vec2 pixelCoords = ndc.xy * numPixels;
+    vec2 pixelCoords = gl_FragCoord.xy * numPixels;
     float pixelIndex = pixelCoords.y * numPixels.x + pixelCoords.x;
 
-    float rngState = pixelIndex + time;
+    uint rngState = uint(pixelIndex + time * 719393.0);
 
     float fov = radians(90.0);
     float focalLength = 1.0 / tan(fov / 2.0);
@@ -186,7 +176,7 @@ void main() {
     vec3 viewPoint = vec3(ndc.x, ndc.y, -focalLength);
     vec3 rayOrigin = vec3(0.0, 0.0, 0.0);
     vec2 jitter = RandomPointInCircle(rngState) * DIVERGENCE / numPixels.x;
-    vec3 jitteredView = viewPoint + vec3(ndc.x) * vec3(jitter.x) + vec3(ndc.y) * vec3(jitter.y);
+    vec3 jitteredView = viewPoint + vec3(1.0, 0.0, 0.0) * vec3(jitter.x) + vec3(0.0, 1.0, 0.0) * vec3(jitter.y);
     vec3 rayDir = normalize(jitteredView - rayOrigin);
 
     Ray ray = Ray(rayOrigin, rayDir);
@@ -199,6 +189,6 @@ void main() {
 
     vec4 accumulatedAverage = oldRender * (1.0 - weight) + newRender * weight;
 
-    FragColor = accumulatedAverage;
+    FragColor = vec4(accumulatedAverage.xyz, 1.0);//vec4(RandomValue(rngState), RandomValue(rngState), RandomValue(rngState), 1.0);
 }
 `
